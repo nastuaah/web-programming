07783610110cd3381dbc9ed13663813a30c619d3
nastuaah <homenko.nastya2004@yandex.ru>
2023-12-03 22:34:06 +0300

    KR1

---
+++ kr2/catalogue.js
@@ -0,0 +1,27 @@
+async function getResponce1() {
+
+    let responce = await fetch("catalogue.json")
+
+    let content = await responce.text()
+    console.log(content)
+    content = JSON.parse(content)
+    content = content.splice(0, 9)
+    console.log(content)
+    let key
+    for (key in content) {
+        console.log(content[key].id, content[key].title)
+        console.log(content[key])
+    }
+
+    let node_for_insert = document.getElementById("node_for_insert")
+    for (key in content) {
+        node_for_insert.innerHTML += `
+        <li style="width: 280px" class="d-flex flex-column m-1 p-1">
+        <img style="width: 280px; height: 350px" class="align-self-center" src=${content[key].img}>
+        <h5 class="card-title" style="text-align: center">${content[key].title}</h5>
+        <input type="hidden" name= "vendor_code" value=${content[key].vendor_code}>
+        </li>
+                `
+    }
+}
+getResponce1()
