1be21df8f68d95236cea5e3c0935e2eab5666ea6
nastuaah <homenko.nastya2004@yandex.ru>
2023-12-04 15:27:30 +0300

    kr2

--- kr2/catalogue.js
+++ kr2/catalogue.js
@@ -16,8 +16,8 @@ async function getResponce1() {
     let node_for_insert = document.getElementById("node_for_insert")
     for (key in content) {
         node_for_insert.innerHTML += `
-        <li style="width: 280px" class="d-flex flex-column m-1 p-1">
-        <img style="width: 280px; height: 350px" class="align-self-center" src=${content[key].img} title=${content[key].imgTitle}>
+        <li style="width: 300px" class="d-flex flex-column m-1 p-1">
+        <img style="width: 300px; height: 350px" class="align-self-center" src=${content[key].img} title=${content[key].imgTitle}>
         <h5 class="card-title" style="text-align: center">${content[key].title}</h5>
         <input type="hidden" name= "vendor_code" value=${content[key].vendor_code}>
         </li>
